d03f10e7ab4bcc3941bfaaf567bc972345e3b9b6
Billy Kong <billyk142@gmail.com>
2018-04-07 16:14:56 -0400

    put updated site on the io

---
+++ js/plugs.js
@@ -0,0 +1,59 @@
+var myChart = document.getElementById("myChart").getContext("2d");
+
+var pieChart= new Chart(myChart,{
+    type:"pie",// bar, horizontalBar, pie, line, doughnut, radar, polarArea
+    data:{
+        labels:["Residential","Commerial","Example 3","Example 4"],
+        datasets:[{
+            label:"Label",
+            data:[
+                55,30,10,5,
+            ],
+            backgroundColor:[
+                "rgba(255, 99, 132, 0.6)",
+                "rgba(54, 162, 235, 0.6)",
+                "rgba(75, 192, 192, 0.6)",
+                "rgba(255, 159, 64, 0.6)",
+            ]
+        }]
+    },
+    options:{
+        legend:{
+            dispaly:true,
+            position:"top",
+        },
+    }
+
+});
+
+//Scripp reveal
+window.sr = ScrollReveal({reset:true,duration: 2000
+                         });
+
+sr.reveal('.c1', {
+    distance:'300px',
+    origin:'left',
+    viewFactor: 0.2
+});
+
+sr.reveal('.c2', {
+
+    distance: '600px',
+    origin:'left',
+    viewFactor: 0.2
+});
+
+sr.reveal('.c3', {
+
+    distance:'900px',
+    origin:'left',
+    viewFactor: 0.2
+});
+
+
+$(document).ready(function(){
+    $('.dropdown').on('mouseenter mouseleave click tap', function(event) {
+        event.preventDefault();
+        $(this).toggleClass("open");
+    });
+});
